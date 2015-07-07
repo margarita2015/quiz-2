667148eb8715b0a2822cbe72bec3df66baf68a83
margarita2015 <mcorreo200@yahoo.es>
2015-07-07 16:08:22 +0200

    Revisión 2-quiz-9 y quiz 10 (autoload)
    Tema 7- Autoload
    sólo se veía una pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,16 +1,16 @@
 var models = require('../models/models.js'); 
 
 //Autoload - factoriza el código si ruta incluye :quizId
-exports.load = function(req, res, next, quizId) {
-	models.Quiz.find(quizId).then(
+exports.load = function(req, res, next, quizId){
+	models.Quiz.findById(quizId).then(
 		function(quiz) {
 			if (quiz) {
 				req.quiz = quiz;
 				next();
-			} else { next(new Error('No existe quizId=' + quizId)); }
+			} else { next(new Error('No existe quizId= ' + quizId)); }
 		}
 		).catch(function(error) { next(error);});
-};
+}
 
 // GET /quizes
 exports.index = function(req, res) { 
@@ -18,13 +18,15 @@ exports.index = function(req, res) {
 		function(quizes) {
 		res.render('quizes/index.ejs', {quizes: quizes});
 	}
-	).catch(function(error) { next(error);})
+	).catch(function(error) {next(error);});
   };
 
 
 // GET /quizes/:id
 exports.show = function(req, res) {
+	models.Quiz.findById(req.params.quizId).then(function(quiz) {
  	res.render('quizes/show', { quiz: req.quiz});
+ })
  };
 
 
@@ -36,6 +38,6 @@ exports.answer = function(req, res) {
 	if (req.query.respuesta === req.quiz.respuesta)  {
 		resultado = 'Correcto';
 	}
-	    res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado});
+	    res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
 	
 	};
