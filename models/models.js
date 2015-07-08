4632c80162ededfcad126dcfa519222b8455fb34
margarita2015 <mcorreo200@yahoo.es>
2015-07-08 14:01:43 +0200

    revision 3-quiz-9 y quiz 10 (autoload)
    Modulo 7

--- models/models.js
+++ models/models.js
@@ -27,7 +27,8 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 );
 
 // Importar definicion de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
 
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
 
