9c69bce7e6b520208da43a1566d3a719416e28c7
margarita2015 <mcorreo200@yahoo.es>
2015-07-09 15:11:50 +0200

    P2P Modulo 7-revision

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var quizController = require('../controllers/quiz_controller'); //introducido fa
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'quiz-2' });
+  res.render('index', { title: 'quiz' });
 });
 
 //Autoload de comandos con :quizId
