da5df6acb66c6d8b89fb0ad971d4933957085061
margarita2015 <mcorreo200@yahoo.es>
2015-08-03 21:55:53 +0200

    Quiz - 18 - Moderación de Comentarios
    Módulo 9

--- models/comment.js
+++ models/comment.js
@@ -2,10 +2,15 @@
 
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define(
-    'Comment',
+    'comment',
     { texto: {
         type:DataTypes.STRING,
         validate: {notEmpty: {msg: "-> Falta Comentario"}}
+       },
+       publicado: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+
       }
     }
   );
